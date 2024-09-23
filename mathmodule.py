40cbc5279e7c2863f0ec8b0c370f2e6dd430c5e9
Seethalakshmi <seethalakshmimr9017@gmail.com>
2024-09-23 15:48:17 +0530

    first commit

---
+++ mathmodule.py
@@ -0,0 +1,11 @@
+import math
+example_variable = math.sqrt(16)
+power_variable = math.pow(2,5)
+ceil_variable = math.ceil(6.3)
+floor_variable = math.floor(5.5)
+fabs_variable = math.fabs
+factorial_variable = math.factorial(4)
+sin_variable = math.sin(math.pi/2)
+euler_variable = math.e
+infinity_variable = math.inf
+print(example_variable)
